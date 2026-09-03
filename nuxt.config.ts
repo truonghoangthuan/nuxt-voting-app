dc8e9f26ed190510c09c1e461a8d85d568d41419
truonghoangthuan <tthun32@gmail.com>
2026-09-03 23:36:22 +0700

    chore: change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ]
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
